4cb08c42d0fac2c3a0873a344caf6a591721a10b
KrazyMiner001 <krazyminer001@outlook.com>
2024-06-26 09:49:28 +0200

    Initial Commit

---
+++ src/main/java/tech/krazyminer001/sapphire/SAPPHIREClient.java
@@ -0,0 +1,10 @@
+package tech.krazyminer001.sapphire;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SAPPHIREClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/tech/krazyminer001/sapphire/SAPPHIREDataGenerator.java
@@ -0,0 +1,11 @@
+package tech.krazyminer001.sapphire;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SAPPHIREDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
