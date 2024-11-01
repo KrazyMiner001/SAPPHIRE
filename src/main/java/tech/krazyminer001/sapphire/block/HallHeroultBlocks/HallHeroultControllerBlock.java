36193b88f409e50fe7cd723d54feabbc3211b09f
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-01 20:23:44 +0200

    Add mutiblock checking code to controller block

--- src/main/java/tech/krazyminer001/sapphire/block/HallHeroultBlocks/HallHeroultControllerBlock.java
+++ src/main/java/tech/krazyminer001/sapphire/block/HallHeroultBlocks/HallHeroultControllerBlock.java
@@ -1,19 +1,44 @@
 package tech.krazyminer001.sapphire.block.HallHeroultBlocks;
 
+import dev.mayaqq.nexusframe.api.multiblock.Multiblock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.predicate.block.BlockStatePredicate;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import tech.krazyminer001.sapphire.block.SapphireBlocks;
+
+import java.util.HashMap;
+import java.util.function.Predicate;
 
 public class HallHeroultControllerBlock extends Block {
+    private static final HashMap<BlockPos, Multiblock> Multiblocks = new HashMap<>();
+
     private static final char[][][] MULTIBLOCK_PATTERN = {
             {
-                    {}
+                    {'$'}
             }
     };
+
+    private static HashMap<Character, Predicate<BlockState>> getPredicates() {
+        HashMap<Character, Predicate<BlockState>> predicates = new HashMap<>();
+        predicates.put('$', BlockStatePredicate.forBlock(SapphireBlocks.HALL_HEROULT_CONTROLLER_BLOCK));
+        return predicates;
+    }
+
+    private static void getMultiblock(BlockPos pos) {
+        if (Multiblocks.get(pos) != null) {
+            Multiblocks.put(pos, new Multiblock(MULTIBLOCK_PATTERN, getPredicates(), true));
+        }
+    }
+
+    public boolean isMultiblockValid(BlockPos pos, World world) {
+        getMultiblock(pos);
+        return Multiblocks.get(pos).check(pos, world);
+    }
 
     public HallHeroultControllerBlock(Settings settings) {
         super(settings);
