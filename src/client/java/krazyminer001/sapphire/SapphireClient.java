7e6525fe82945e12956860f5e2ba744c40759022
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-22 13:10:51 +0200

    add template

---
+++ src/client/java/krazyminer001/sapphire/SapphireClient.java
@@ -0,0 +1,10 @@
+package krazyminer001.sapphire;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SapphireClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/krazyminer001/sapphire/SapphireDataGenerator.java
@@ -0,0 +1,11 @@
+package krazyminer001.sapphire;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SapphireDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
